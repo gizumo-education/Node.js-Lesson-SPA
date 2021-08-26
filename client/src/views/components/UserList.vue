b71281f3dc6e6b255229a5b4dbaa7406606c40ca
kousuke <firstdouga@gmail.com>
2021-08-26 17:42:38 +0900

    feat: ユーザー一覧表示機能追加。

---
+++ client/src/views/components/UserList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div v-for="user in userList"
+      :key="user.id">
+      <p>ユーザーのid：{{ user.id }}</p>
+      <p>ユーザーの名前：{{ user.username }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  created() {
+    this.getUserList();
+  },
+  methods: {
+    ...mapActions({
+      getUserList: 'updateUserList',
+    }),
+  },
+  computed: {
+    ...mapGetters([
+      'userList',
+    ]),
+  },
+};
+</script>
